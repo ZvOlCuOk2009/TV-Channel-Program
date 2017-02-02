629c4d800f1a40478bc40a5392179ab6a66fff22
Zvigun Alexandr <mircamnia@ukr.net>
2017-02-02 11:58:20 +0200

    Initial Commit

---
+++ TV Channel Program/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TV Channel Program
+//
+//  Created by Mac on 02.02.17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
